3e8e5e9b9eefb2bf0d6b8bd53dbefad48fa4a08c
Linus Aspelöf <Tederollz@hotmail.com>
2023-11-21 13:56:56 +0100

    Add project files.

---
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

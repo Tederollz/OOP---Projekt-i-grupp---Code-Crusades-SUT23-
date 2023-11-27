01372b2b9aac35d4a667ccb1867bfacbdcfbe5d5
Christian Rapp <christian.rapp@edu.varberg.se>
2023-11-27 09:28:53 +0100

    skapat lite classer

---
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/Accounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
+{
+    internal class Accounts
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+
+        public Accounts(string name, decimal balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+    }
+}
---
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        public string Pin { get; set; }
+        public List<Accounts> Accounts { get; set; }
+        public bool Role { get; set; }
+
+        public User(string username, string pin, bool role)
+        {
+            Username = username;
+            Pin = pin;
+            Accounts = new List<Accounts>();
+            Role = role;
+        }
+    }
+
+
+
+
+
+
+}

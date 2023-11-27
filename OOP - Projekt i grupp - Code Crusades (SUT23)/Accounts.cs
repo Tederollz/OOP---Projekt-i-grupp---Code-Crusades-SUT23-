23b8edc56d8789e8f59f13b6168b39fe6f0be1e7
Jonathanjohansson24 <jonathan.nordin24@gmail.com>
2023-11-27 11:13:18 +0100

    Made two methods one for creating regular account and one for Savings

--- OOP - Projekt i grupp - Code Crusades (SUT23)/Accounts.cs
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/Accounts.cs
@@ -11,10 +11,22 @@ namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
         public string Name { get; set; }
         public decimal Balance { get; set; }
 
-        public Accounts(string name, decimal balance)
+        public Accounts(string name, decimal balance, decimal interestRate = 0)
         {
             Name = name;
             Balance = balance;
+        }
+        
+        public static Accounts CreateAccount(string name, decimal insert)
+        {
+            name = "CheckingAccount";
+            return new Accounts(name, insert);
+        }
+        public static Accounts CreateSavingsAccount(string name, decimal insert)
+        {
+            name = "SavingsAccount";
+
+            return new Accounts(name, insert);
         }
     }
 }
--- OOP - Projekt i grupp - Code Crusades (SUT23)/Customer.cs
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/Customer.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
 {
-    internal class Customer
+    internal class Customer 
     {
+        
     }
 }

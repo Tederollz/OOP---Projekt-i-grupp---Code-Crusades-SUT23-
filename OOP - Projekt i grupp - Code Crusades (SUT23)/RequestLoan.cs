9857c105b0f1708ab645b6a34db088b373154127
Olof Sandberg <olof.sandberg1990@gmail.com>
2023-11-28 10:22:34 +0100

    New branch, Olof 2.0

---
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/RequestLoan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
+{
+    internal class RequestLoan
+    {
+        public static decimal maxAmount(Customer customer)
+        {
+
+            decimal totalCapital = customer.Accounts.Sum(account => account.Balance);
+            decimal maxLoan = totalCapital * 5;
+            return maxLoan;
+
+
+        }
+    }
+}

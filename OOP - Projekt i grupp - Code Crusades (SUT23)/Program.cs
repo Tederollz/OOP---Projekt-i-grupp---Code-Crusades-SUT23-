5179676ad4c0c7825444cda053a0b366851dc403
Linus Aspelöf <Tederollz@hotmail.com>
2023-11-28 09:51:37 +0100

    Fix login again

--- OOP - Projekt i grupp - Code Crusades (SUT23)/Program.cs
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Accounts.Login();
+            User.Login();
         }
     }
 }
--- OOP - Projekt i grupp - Code Crusades (SUT23)/User.cs
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/User.cs
@@ -18,6 +18,43 @@ namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
             Pin = pin;
             Role = role;
         }
+        public static void Login()
+        {
+            List<Customer> customers = Customer.CreateCustomerList();
+            int loginAttempts = 0;
+            User currentUser = null;
+
+            Console.Clear();
+            Console.WriteLine("\n\tVälkommen till Bankomaten!");
+            while (loginAttempts < 3)
+            {
+                Console.Write("\n\tAnge ditt Användar-ID: ");
+                string username = Console.ReadLine();
+
+                Console.Write("\n\tAnge din pinkod: ");
+                string pin = Console.ReadLine();
+
+                User UserLogin = customers.Find(u => u.Username == username && u.Pin == pin);
+
+                if (UserLogin != null)
+                {
+                    currentUser = UserLogin;
+                    loginAttempts = 0;
+                    Console.WriteLine($"\n\tInloggning lyckades. Välkommen!" +
+                        "\n\tTryck \"Enter\" för att Fortsätta ");
+                    Console.ReadKey();
+                    //Menu();
+                }
+                else
+                {
+                    Console.WriteLine("\n\tFelaktigt användarnamn eller pinkod. Försök igen.");
+                    loginAttempts++;
+                }
+            }
+            Console.WriteLine("\n\tFör många felaktiga inloggningsförsök. " +
+                "\n\tProgrammet avslutas....");
+            Console.ReadKey();
+        }
     }
 
 

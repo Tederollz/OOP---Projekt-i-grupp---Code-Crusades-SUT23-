17eb1f3a4f1c80a181b3e0229e04551020dea84f
Linus Aspelöf <Tederollz@hotmail.com>
2023-11-28 14:27:21 +0100

    Bytt namn på meny metod

---
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/PrintAccounts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
+{
+    internal class PrintAccounts : Customer
+    {
+        public PrintAccounts(string username, string pin, bool role) : base(username, pin, role)
+        {
+        }
+        public void PrintAcc()
+        {
+           /* Console.WriteLine("\n\tDina konton & saldo:");
+
+            foreach (var account in User.CurrentUser)
+            {
+                Console.WriteLine($"\n\t{account.Name}: {account.Balance:C}");
+            }
+           */
+        }
+    }
+}

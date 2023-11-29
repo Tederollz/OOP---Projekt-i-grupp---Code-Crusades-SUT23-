12c2c3d0645a4036307b1647d610e541e90b65c0
Christian Rapp <christian.rapp@edu.varberg.se>
2023-11-29 10:33:58 +0100

    såja

--- OOP - Projekt i grupp - Code Crusades (SUT23)/Transfer.cs
+++ OOP - Projekt i grupp - Code Crusades (SUT23)/Transfer.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
 {
-    /*
+
     internal class Transfer : Customer
     { // behöver ändras om lite i customerklassen där kontonen finns eller mina source och destination för att träffa rätt.
         public decimal Balance { get; set; }
         public string SourceAccountType { get; set; }
         public string DestinationAccountType { get; set; }
 
-        public Transfer(string sourceAccountType, string destinationAccountType, decimal balance): base("TransferCustomer", "", false)
+        public Transfer(string sourceAccountType, string destinationAccountType, decimal balance) : base("TransferCustomer", "", false)
         {
             SourceAccountType = sourceAccountType;
             DestinationAccountType = destinationAccountType;
@@ -22,10 +22,10 @@ namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
 
         public void TransferMoney(Customer sourceCustomer)
         {
-            // Hitta källkonto
+            Hitta källkonto
             Accounts sourceAccount = sourceCustomer.Accounts.Find(acc => acc.Name == SourceAccountType);
 
-            // Hitta målkonto
+            Hitta målkonto
             Accounts destinationAccount = Accounts.Find(acc => acc.Name == DestinationAccountType);
 
             if (sourceAccount != null && destinationAccount != null && Balance > 0 && sourceAccount.Balance >= Balance)
@@ -40,5 +40,5 @@ namespace OOP___Projekt_i_grupp___Code_Crusades__SUT23_
             }
         }
     }
-    */
+
 }
